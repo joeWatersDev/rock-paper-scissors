a6658504debe5a15ceba4004d0c73f798913c14d
Joe Seger <joesegerdev@gmail.com>
2022-10-06 15:09:05 -0400

    Create function to run game and track scores

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 const moves = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
-    return moves[Math.floor(Math.random()*4)]
+    return moves[Math.floor(Math.random()*3)]
 }
 
 function playRound(playerSelection, computerSelection){
@@ -11,12 +11,35 @@ function playRound(playerSelection, computerSelection){
     let cIndex = moves.indexOf(computerSelection);
 
     if (pIndex == (cIndex + 1)%3){
-        return "You win! " + playerLower[0].toUpperCase() + playerLower.slice(1) + " beats " + computerSelection + ".";
+        console.log("You win! " + playerLower[0].toUpperCase() + playerLower.slice(1) + " beats " + computerSelection + ".");
+        return "player";
     }
 
     else if (pIndex == cIndex){
-        return "Tie!";
+        console.log("Tie!");
+        return "tie";
     }
 
-    else return "You lose! " + computerSelection[0].toUpperCase() + computerSelection.slice(1) + " beats " + playerLower +".";
+    else{ 
+        console.log("You lose! " + computerSelection[0].toUpperCase() + computerSelection.slice(1) + " beats " + playerLower +".");
+        return "computer";
+    }
+}
+
+function game(){
+    let userMove = prompt("What move would you like to use? Rock, paper, or scissors?: ");
+    let computerMove = getComputerChoice();
+    let winner = playRound(userMove, computerMove);
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    if (winner == "player") {
+        playerScore += 1
+    }
+
+    else if (winner == "computer") {
+        computerScore += 1
+    }
+
 }
